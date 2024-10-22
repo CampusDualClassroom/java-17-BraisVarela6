09599cfaba95c05fddc2d5af4b6332612e1c1afb
BraisVarela <braisvarela6@gmail.com>
2024-10-22 17:28:36 +0200

    Ejercicio 17 Hecho

--- src/main/java/com/campusdual/classroom/Exercise17.java
+++ src/main/java/com/campusdual/classroom/Exercise17.java
@@ -1,15 +1,35 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
+
+import java.util.Arrays;
+
 public class Exercise17 {
-	public static int[] createEmptyIntArray(int positions) {}
 
-	public static String[] createInitializedStringArray() {}
 
-	public static void populateArray(int[] array, int index, int value) {}
 
-	public static int arrayLength(int[] array) {}
+	public static int[] createEmptyIntArray(int positions){
+        return new int[positions];
 
-	public static void showValue(int[] array, int index) {}
+    }
+
+	public static String[] createInitializedStringArray() {
+        return new String[] {"1", "2", "3"};
+
+    }
+
+	public static void populateArray(int[] array, int index, int value) {
+        array[index] = value;
+    }
+
+	public static int arrayLength(int[] array) {
+		System.out.println("La longitud del array es :" + array.length);
+        return array.length;
+    }
+
+	public static void showValue(int[] array, int index) {
+		System.out.println("Valor del índice " + index + ": " + array[index]);
+	}
 
 	public static void main(String[] args) {
 		int[] intArray = createEmptyIntArray(3);
